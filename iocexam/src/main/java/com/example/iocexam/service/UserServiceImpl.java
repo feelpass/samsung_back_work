9be2c1293ededdb0fc6ed62ebc20c7cc96103ef5
carami <carami@nate.com>
2023-09-05 11:14:00 +0900

    update exam

--- iocexam/src/main/java/com/example/iocexam/service/UserServiceImpl.java
+++ iocexam/src/main/java/com/example/iocexam/service/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package com.example.iocexam.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import com.example.iocexam.dao.UserDao;
@@ -8,11 +10,24 @@ import com.example.iocexam.domain.User;
 @Service
 public class UserServiceImpl implements UserService {
 	
+//	@Autowired
+//	@Qualifier("userDaoImpl")
 	private UserDao userDao;
 	
-	public UserServiceImpl(UserDao userDao) {
+	public UserServiceImpl() {
+		
+	}
+	
+	
+//	public UserServiceImpl(UserDao caramiDAO) {
+//		this.userDao = caramiDAO;
+//	}
+	@Autowired
+	@Qualifier("userDaoImpl")
+	public void setUserDao(UserDao userDao) {
 		this.userDao = userDao;
 	}
+
 
 	@Override
 	public void joinUser(User user) {

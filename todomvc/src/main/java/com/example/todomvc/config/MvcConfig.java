1974e4dc90ca4e99a03003abb05652addca37885
carami <carami@nate.com>
2023-09-06 17:28:18 +0900

    update todomvc

---
+++ todomvc/src/main/java/com/example/todomvc/config/MvcConfig.java
@@ -0,0 +1,20 @@
+package com.example.todomvc.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+@ComponentScan(basePackages = "com.example.todomvc.controller")
+@EnableWebMvc
+public class MvcConfig implements WebMvcConfigurer {
+
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		registry.jsp("/WEB-INF/views/", ".jsp");
+	}
+
+}

c1e13befe11faf869df9b3845c19f3aa69ac209c
carami <carami@nate.com>
2023-09-07 15:51:35 +0900

    update jpaexam

--- jpaexam/src/main/java/jpaexam/JpaRun.java
+++ jpaexam/src/main/java/jpaexam/JpaRun.java
@@ -18,18 +18,59 @@ public class JpaRun {
 
 			// 1. 저장.
 
-			Product product = new Product(); // 엔티티 생성!!
-//			product.setId(2L);
-			product.setName("note");
-			product.setPrice(5000); // 여기까지는 비영속 상태!!
+//			Product product = new Product(); // 엔티티 생성!!
+////			product.setId(2L);
+//			product.setName("note");
+//			product.setPrice(5000); // 여기까지는 비영속 상태!!
+//
+//			System.out.println("persist()  실행 전!!");
+//			em.persist(product); // 영속상태. 영속상태에 들어간다고 해서, DB에 바로 반영되는것은 아님.
+//			System.out.println("persist()  실행 후!!");
+//
+//			
 
-			System.out.println("persist()  실행 전!!");
-			em.persist(product); // 영속상태. 영속상태에 들어간다고 해서, DB에 바로 반영되는것은 아님.
-			System.out.println("persist()  실행 후!!");
+			// 2. 조회
+//			System.out.println("find()  실행 전!!");
+//			Product findProduct =	em.find(Product.class, 129L );
+//			System.out.println("find()  실행 후!!");
+//			System.out.println(findProduct);
+//			
+//			
+//			System.out.println("find()  실행 전!!");
+//			Product findProduct2 =	em.find(Product.class, 129L );
+//			System.out.println("find()  실행 후!!");
+//			System.out.println(findProduct2);
+//			
+//			if(findProduct == findProduct2)
+//				System.out.println("같은 인스턴스입니다.");
+//			
+//			
 
+
+			// 3. 수정
+			Product updateProduct = em.find(Product.class, 129L);
+			updateProduct.setName("aaaa");
+			updateProduct.setPrice(50000);
+			
+			//다른일들이 진행!! 
+			
+			updateProduct.setName("new note");
+			updateProduct.setPrice(2000);
+			
+
+			//4. 삭제 
+			Product delProduct = em.find(Product.class, 129L);
+			
+			em.remove(delProduct);
+			
+			
+			
 			System.out.println("commit()  실행 전!!");
 			tx.commit();
 			System.out.println("commit()  실행 후!!");
+			
+			
+
 		} catch (Exception e) {
 			tx.rollback();
 		}
